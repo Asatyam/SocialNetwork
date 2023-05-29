645adf8b266e838accb17e0969212b09d8be6da3
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-29 18:29:37 +0530

    Style the login page textbox

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,7 +3,9 @@ import { useState,useContext } from "react";
 import { AuthContext } from "./context";
 import axios from "axios";
 import styles from '../styles/Login.module.css'
-
+import { Roboto } from "next/font/google";
+const roboto = Roboto({subsets:['latin'],weight:'500'});
+const robotoBold = Roboto({subsets:['latin'],weight:'900'});
 export default function Login(){
 
     const [email,setEmail] = useState("");
@@ -12,8 +14,11 @@ export default function Login(){
     const {auth, setAuth,user,setUser} = useContext(AuthContext);
 
     return(
-       <div className={styles['main']}>
-            <div className={styles['text-box']}></div>
+       <div className={`${styles['main']} ${roboto.className}` }>
+            <div className={styles['text-box']}>
+                <h1 className={robotoBold.className}>Odinbook</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+            </div>
             <div className={styles['form-container']}></div>
        </div>
     )
