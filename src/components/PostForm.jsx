cc3c5663735567db3bcf525f1a0cfc41c758392e
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-06-01 14:00:03 +0530

    Add the ability to post images

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -7,23 +7,35 @@ import axios from "axios";
 export default function PostForm({setShowForm}){
 
     const [content, setContent] = useState("");
+    const [file,setFile] = useState(null);
 
     const closeForm = (e)=>{
         setShowForm(false);
     }
     const handleSubmit = (e)=>{
+        const formData = new FormData();
+        formData.set('content',content);
+        
+        if(file){ 
+            formData.append('file',file);  
+        }
         e.preventDefault();
          const token = JSON.parse(localStorage.getItem('token'));
            const config = {
-                headers: {Authorization: `Bearer ${token}`}
+                headers: {Authorization: `Bearer ${token}`},
+                'Content-Type': 'multipart/form-data'
            }
-           axios.post(`http://localhost:4000/api/posts`,{content},config)
+           axios.post(`http://localhost:4000/api/posts`,formData,config)
            .then((res)=>{
                 console.log(res);
                 setContent('');
                 setShowForm(false);
            })
            .catch(console.log);
+    }
+    const handleFileChange = (e)=>{
+        setFile(e.target.files[0]);
+        console.log(e.target.files[0]);
     }
     return(
         <div className = {styles['container']}>
@@ -32,11 +44,11 @@ export default function PostForm({setShowForm}){
                 <button className='icon' onClick={closeForm}><img src='../../images/cancel.png' alt = 'close-btn'/></button>
             </div>
             
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
                 <textarea required name = 'post' id='post' value = {content} onChange = {(e)=>setContent(e.target.value)} placeholder="What's on your mind?"></textarea>
                 <div className="img-input">
                     <label htmlFor="image"><img src='../../images/gallery.png' alt = 'add-image-btn'/></label>
-                    <input type="file" id='image' size='60' name='postImage' />
+                    <input type="file" id='image' size='60' name='postImage' onChange={handleFileChange} />
                 </div>
                 <button type="submit">Create</button>
             </form>
