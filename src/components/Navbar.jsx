006c624a2ed5ee40272d2bbb529500de7aa1e6a5
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-06-03 21:21:09 +0530

    Implement the search functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useContext, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useState } from "react";
 import styles from '../styles/Navbar.module.css'
 import Link from "next/link";
@@ -12,12 +12,51 @@ export default function Navbar(){
 
 
     const [search, setSearch] = useState('');
+    const [users,setUsers] = useState([]);
     const {setAuth} = useContext(AuthContext);
+    const [results,setResults] = useState([]);
+    const [showResults, setShowResults] = useState(true);
     const router = useRouter();
     let userid;
     if(typeof window !=='undefined'){
         const user = JSON.parse(localStorage.getItem('user'));
         userid = user;
+    }
+    useEffect(()=>{
+         const token = JSON.parse(localStorage.getItem('token'));
+           const config = {
+                headers: {Authorization: `Bearer ${token}`}
+           }
+        axios.get('http://localhost:4000/api/users',config)
+        .then((res)=>{
+            console.log(res.data.users);
+            setUsers(res.data.users);
+        }) 
+        .catch(console.log);
+
+        
+    },[])
+    const searchUsers = (e)=>{
+        setSearch(e.target.value);
+        setShowResults(true);
+        setResults((r)=>[]);
+        if(e.target.value.length>0){
+            for(let i=0;i<users.length;i++){
+                const searchValue =  e.target.value.toLowerCase();
+                const first_name = users[i].first_name.toLowerCase();
+                const last_name = users[i].last_name.toLowerCase();
+                const condition1 = first_name.includes(searchValue);
+                const condition2 = last_name.includes(searchValue);
+                if(condition1 || condition2){
+                    setResults(r=>[...r,users[i]])
+                }
+                console.log({searchValue,condition1,condition2, first_name,last_name})
+               
+            }
+        }
+        console.log(results);
+         console.log('-------------------------------------');
+       
     }
     const handleLogout = (e)=>{
         const token = JSON.parse(localStorage.getItem('token'));
@@ -33,14 +72,26 @@ export default function Navbar(){
     return(
             <nav className={styles['main-nav']}>
                 <h1><Link href = '/'>Odinbook</Link></h1>
-                <div className={`${styles['search']} ${styles['wide']}}`}>
-                    <input type="search" name='search' value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Search"/>
+                <div className={styles['search']}>
+                    <div className={styles['search-box']}>
+                         <input type="search" name='search' value={search} onChange={searchUsers} placeholder="Search"/>
+                   </div>
+                    {results.length>0 && showResults && <div className={styles['search-results']}>
+                    {results.map(
+                    result=>(
+                         <p key={result._id} onClick={(e)=>setShowResults(false)}>
+                            <Link href={`users/${result._id}`}>{result.first_name + ' ' + result.last_name}</Link>
+                        </p>
+                    ))}
+                </div>
+                }
                 </div>
                 <nav className = {styles['small-nav']}>
                     <button className='icon'><img src="/images/chat.png" alt = "Chat-icon"/></button>
                     <button className = 'icon' onClick={handleLogout}><img src="/images/logout.png" alt = "logout-icon"/></button>
                     <button className = 'icon'><Link href = {`/users/${userid}`}><img src="/images/profile.png" alt = "profile-icon"/></Link></button>
-                </nav> 
+                </nav>
+              
             </nav>
     )
 }
